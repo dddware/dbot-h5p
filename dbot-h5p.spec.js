11a1693580a5026839f4f5c37473f7960eebe3d8
neemzy <tom.panier@free.fr>
2014-07-05 13:27:26 +0200

    fixed tests

--- dbot-h5p.spec.js
+++ dbot-h5p.spec.js
@@ -1,5 +1,4 @@
-var AsyncSpec = require('jasmine-async')(jasmine)
-  , h5p = require('./dbot-h5p');
+var h5p = require('./dbot-h5p');
 
 // Mock regex matches
 var request = 'fontface'
@@ -7,10 +6,7 @@ var request = 'fontface'
   , expectation = "fontface:\nInternet Explorer: 9\nFirefox: 3.5\nChrome: 4\nSafari (desktop): 3.2\nOpera (desktop): 10.0-10.1\nSafari (iOS): 4.2-4.3\nOpera (mobile): 10\n";
 
 describe('h5p', function () {
-  var async = new AsyncSpec(this)
-    , asyncResult;
-
-  async.it('should retrieve expected results', function (done) {
+  it('should retrieve expected results', function (done) {
     h5p.callback(matches).then(function (result) {
       expect(result).toEqual(expectation);
 
